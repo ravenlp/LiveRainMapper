0bf85fcb8346eed2328c3ec3cd3188121c62f112
Jorge Condomi <ravenlp@gmail.com>
2013-11-27 03:36:46 -0300

    "map, weather and tweets showing"

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,7 +1,28 @@
 'use strict';
 
 angular.module('rainMapper')
-    .controller('MainCtrl', ['$scope', 'socket', function ($scope, socket) {
+    .controller('MainCtrl', ['$scope', 'socket','$timeout', function ($scope, socket, $timeout) {
+
+        $scope.layers =  {
+            baselayers: {
+                googleTerrain: {
+                    name: 'Google Terrain',
+                        layerType: 'TERRAIN',
+                        type: 'google'
+                },
+                googleHybrid: {
+                    name: 'Google Hybrid',
+                        layerType: 'HYBRID',
+                        type: 'google'
+                },
+                googleRoadmap: {
+                    name: 'Google Streets',
+                        layerType: 'ROADMAP',
+                        type: 'google'
+                }
+            }
+        };
+
 
         $scope.tweets = [];
 
@@ -12,28 +33,38 @@ angular.module('rainMapper')
                 zoom: 5
             },
             markers: {
-                'asdasdasdasd': {lat: -99.55127064, lon: 27.44663564, focus: true, draggable: false, message:'HOLA CHE'}
+                's': {lat: -27.74, lng: -57.17, focus: true, draggable: false, message:'HOLA CHE'}
             }
         };
+
+        var validatorsLayer = new OsmJs.Weather.LeafletLayer({lang: 'en'});
+
+        //map.addLayer(validatorsLayer);
+        $timeout(function(){
+
+            $scope.$broadcast('leafletDirectiveSetMap', ['addLayer', validatorsLayer]);
+        }, 1000);
 
         // Incoming data
         var i = 0;
         socket.on('data', function(data) {
-
             if(data && angular.isArray(data)){
                 angular.forEach(data, function(t,i){
                     if(t.coordinates){
                         $scope.tweets.push(t);
-                        $scope.map.markers[t.id_srt] = {
-                            lat: t.coordinates.coordinates[0],
-                            lon: t.coordinates.coordinates[1],
+                        $scope.map.markers[t.id_str] = {
+                            lat: t.coordinates.coordinates[1],
+                            lng: t.coordinates.coordinates[0],
                             focus: true,
-                            draggable: false
+                            draggable: false,
+                            message: t.text
                         };
                         console.log($scope.map.markers[t.id_srt]);
                     }
                 });
             }
+
+
 
             console.log(i++, "DATA", data);
         });
